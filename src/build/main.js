e2633eee58e8ea4d5e9636c097c724ff7889edb9
Molmin <milmon@qq.com>
2023-03-04 07:47:13 +0800

    fix

--- src/build/main.js
+++ src/build/main.js
@@ -1,3 +1,5 @@
 const fs=require('fs');
 if(!fs.existsSync("dist"))fs.mkdirSync("dist");
-fs.writeFileSync("dist/index.html",fs.readFileSync("data/round1.yaml",'utf8',(err,data)=>{}),(err)=>{});
+fs.writeFileSync("dist/index.html",fs.readFileSync("data/round2.yaml",'utf8',(err,data)=>{}),(err)=>{});
+var ghpages=require('gh-pages');
+ghpages.publish('dist',function(err){});
